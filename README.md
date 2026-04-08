e8349adc8604139ffab4c8e55524fb041b138a3b
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-08 18:03:40 +0200

    second release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sshmgr
 A bash based ssh host manager for managing ssh connections
+
+For instllation cd into the cloned repo and run ./INSTALL. The rest should be done automaticly.
--- sshmgr.sh
+++ sshmgr.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 HOSTS_FILE="$HOME/.local/bin/sshmgr/known_hosts.save"
 
@@ -9,19 +8,20 @@ case "$1" in
         # wird ausgeführt wenn $1 "-e" oder "--edit" ist
 	echo "opening known hosts file with nano..."
 	nano "$HOSTS_FILE"
-        ;;         # <-- das ist wie "break" in Python
+    ;;         # <-- das ist wie "break" in Python
     -u|--uninstall)
         # wird ausgeführt bei "-u" oder "--uninstall"
 	echo "uninstalling sshmgr..."
 	
 	echo "removing directory $HOME/.local/bin/sshmgr..."
-	sudo rm -rf "$HOME/.local/bin/sshmgr" 
+	rm -rf "$HOME/.local/bin/sshmgr" 
 
 	echo "removing alias from .bashrc file in home directory..."
-	sed -i '/alias sshmgr/d' $HOME/.bashrc   	
+	sed -i '/alias sshmgr/d' $HOME/.bashrc
+	echo "note that you need to reopen the terminal to apply"
 	;;
     -h|--help)
-	    echo -e "Usage:\n \nsshmgr | opens host selection. exit py pressing CTRL+Q\n \nsshmgr -e / sshmgr --edit | opens the known hosts file with nano for you to edit it\n \nsshmgr -u / sshmgr --uninstall | uninstalls sshmgr (needs to be ran with sudo)\n \nsshmgr -h / sshmgr --help | shows this text for help"
+	echo -e "Usage:\n \nsshmgr | opens host selection. exit py pressing CTRL+Q\n \nsshmgr -e / sshmgr --edit | opens the known hosts file with nano for you to edit it\n \nsshmgr -u / sshmgr --uninstall | uninstalls sshmgr (needs to be ran with sudo)\n \nsshmgr -h / sshmgr --help | shows this text for help"
 	;;
     "")
 	echo "opening known host selection..."
@@ -34,9 +34,9 @@ case "$1" in
 
 	echo "Verbinde mit $name ($user_ip)..."
 	ssh "$user_ip"
-        ;;
+    ;;
     *)
-        # der "default" Fall — unbekanntes Argument
-        echo -e "unknown option: $1\nrun sshmgr -h for available options"
-        ;;
+    # der "default" Fall — unbekanntes Argument
+    echo -e "unknown option: $1\nrun sshmgr -h for available options"
+    ;;
 esac   # <-- "case" rückwärts geschrieben, so endet der Block
