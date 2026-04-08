d5f467034ee425a31843da5dfd3ef387225087c5
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-08 17:01:13 +0200

    first release

--- sshmgr.sh
+++ sshmgr.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 set -e
 
-host_path="$HOME/.local/bin/sshmgr/known_hosts.save"
+HOSTS_FILE="$HOME/.local/bin/sshmgr/known_hosts.save"
 
 
 case "$1" in
     -e|--edit)
         # wird ausgeführt wenn $1 "-e" oder "--edit" ist
 	echo "opening known hosts file with nano..."
-	nano "$host_path"
+	nano "$HOSTS_FILE"
         ;;         # <-- das ist wie "break" in Python
     -u|--uninstall)
         # wird ausgeführt bei "-u" oder "--uninstall"
 	echo "uninstalling sshmgr..."
-        ;;
+	
+	echo "removing directory $HOME/.local/bin/sshmgr..."
+	sudo rm -rf "$HOME/.local/bin/sshmgr" 
+
+	echo "removing alias from .bashrc file in home directory..."
+	sed -i '/alias sshmgr/d' $HOME/.bashrc   	
+	;;
+    -h|--help)
+	    echo -e "Usage:\n \nsshmgr | opens host selection. exit py pressing CTRL+Q\n \nsshmgr -e / sshmgr --edit | opens the known hosts file with nano for you to edit it\n \nsshmgr -u / sshmgr --uninstall | uninstalls sshmgr (needs to be ran with sudo)\n \nsshmgr -h / sshmgr --help | shows this text for help"
+	;;
     "")
 	echo "opening known host selection..."
+	selected=$(grep -v '^\s*#\|^\s*$' "$HOSTS_FILE" | fzf --prompt="SSH > ")
+
+	[[ -z "$selected" ]] && exit 0
+
+	user_ip="${selected%% - *}"
+	name="${selected#* - }"
+
+	echo "Verbinde mit $name ($user_ip)..."
+	ssh "$user_ip"
         ;;
     *)
         # der "default" Fall — unbekanntes Argument
-        echo "Unbekannte Option: $1"
+        echo -e "unknown option: $1\nrun sshmgr -h for available options"
         ;;
 esac   # <-- "case" rückwärts geschrieben, so endet der Block
