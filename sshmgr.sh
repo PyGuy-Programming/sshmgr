034c2f3967b8e62ac13e81137593245d5b3f22e7
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-20 14:21:56 +0200

    commit

--- sshmgr.sh
+++ sshmgr.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-HOSTS_FILE="$HOME/.local/bin/sshmgr/known_hosts.save"
-
+HOSTS_FILE="${SSHMGR_HOSTS:-$HOME/.config/sshmgr/known_hosts.save}"
 
 case "$1" in
-    -e|--edit)
-        # wird ausgeführt wenn $1 "-e" oder "--edit" ist
-	echo "opening known hosts file with nano..."
-	nano "$HOSTS_FILE"
-    ;;         # <-- das ist wie "break" in Python
-    -u|--uninstall)
-        # wird ausgeführt bei "-u" oder "--uninstall"
-	echo "uninstalling sshmgr..."
-	
-	echo "removing directory $HOME/.local/bin/sshmgr..."
-	rm -rf "$HOME/.local/bin/sshmgr" 
+-e | --edit)
+  # wird ausgeführt wenn $1 "-e" oder "--edit" ist
+  echo "opening known hosts file with nano..."
+  nano "$HOSTS_FILE"
+  ;; # <-- das ist wie "break" in Python
+-u | --uninstall)
+  echo "uninstalling sshmgr..."
 
-	echo "removing alias from .bashrc file in home directory..."
-	sed -i '/alias sshmgr/d' $HOME/.bashrc
-	echo "note that you need to reopen the terminal to apply"
-	;;
-    -h|--help)
-	echo -e "Usage:\n \nsshmgr | opens host selection. exit py pressing CTRL+Q\n \nsshmgr -e / sshmgr --edit | opens the known hosts file with nano for you to edit it\n \nsshmgr -u / sshmgr --uninstall | uninstalls sshmgr (needs to be ran with sudo)\n \nsshmgr -h / sshmgr --help | shows this text for help"
-	;;
-    "")
-	echo "opening known host selection..."
-	selected=$(grep -v '^\s*#\|^\s*$' "$HOSTS_FILE" | fzf --prompt="SSH > ")
+  echo "removing directory $HOME/.local/bin/sshmgr..."
+  rm -rf "$HOME/.local/bin/sshmgr"
 
-	[[ -z "$selected" ]] && exit 0
+  echo "removing config directory $HOME/.config/sshmgr..."
+  rm -rf "$HOME/.config/sshmgr"
 
-	user_ip="${selected%% - *}"
-	name="${selected#* - }"
+  echo "removing alias from .bashrc file in home directory..."
+  sed -i '/alias sshmgr/d' $HOME/.bashrc
+  echo "note that you need to reopen the terminal to apply"
+  ;;
+-h | --help)
+  echo -e "Usage:\n \nsshmgr | opens host selection. exit py pressing CTRL+Q\n \nsshmgr -e / sshmgr --edit | opens the known hosts file with nano for you to edit it\n \nsshmgr -u / sshmgr --uninstall | uninstalls sshmgr (needs to be ran with sudo)\n \nsshmgr -h / sshmgr --help | shows this text for help"
+  ;;
+"")
+  echo "opening known host selection..."
+  selected=$(grep -v '^\s*#\|^\s*$' "$HOSTS_FILE" | fzf --prompt="SSH > ")
 
-	echo "Verbinde mit $name ($user_ip)..."
-	ssh "$user_ip"
-    ;;
-    *)
-    # der "default" Fall — unbekanntes Argument
-    echo -e "unknown option: $1\nrun sshmgr -h for available options"
-    ;;
-esac   # <-- "case" rückwärts geschrieben, so endet der Block
+  [[ -z "$selected" ]] && exit 0
+
+  user_ip="${selected%% - *}"
+  name="${selected#* - }"
+
+  echo "Verbinde mit $name ($user_ip)..."
+  ssh "$user_ip"
+  ;;
+*)
+  # der "default" Fall — unbekanntes Argument
+  echo -e "unknown option: $1\nrun sshmgr -h for available options"
+  ;;
+esac # <-- "case" rückwärts geschrieben, so endet der Block
